4addbd2aef7c7fdffaedbccc592accc38cab6bc9
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-29 11:36:39 +0530

    Update anime.py

--- haruka/modules/anime.py
+++ haruka/modules/anime.py
@@ -4,7 +4,7 @@ from telegram.ext import CallbackQueryHandler, run_async
 from telegram import Message, Update, Bot, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram import ParseMode
 
-from haruka import dispatcher, OWNER_ID, SUDO_USERS, DEV_USERS
+from haruka import dispatcher, OWNER_ID, SUDO_USERS, SUPORT_USERS
 from haruka.modules.disable import DisableAbleCommandHandler
 
 info_btn = "More Information"
