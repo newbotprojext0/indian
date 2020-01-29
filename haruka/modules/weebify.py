62c876b5ee81b54cdf02acfbc007f68b48ff4d85
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-29 11:58:03 +0530

    Update weebify.py

--- haruka/modules/weebify.py
+++ haruka/modules/weebify.py
@@ -1,7 +1,7 @@
 from telegram.ext import run_async, Filters
 from telegram import Message, Chat, Update, Bot, MessageEntity
-from tg_bot import dispatcher
-from tg_bot.modules.disable import CommandHandler
+from haruka import dispatcher
+from haruka.modules.disable import CommandHandler
 
 normiefont = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 weebyfont = ['卂','乃','匚','刀','乇','下','厶','卄','工','丁','长','乚','从','𠘨','口','尸','㔿','尺','丂','丅','凵','リ','山','乂','丫','乙']
