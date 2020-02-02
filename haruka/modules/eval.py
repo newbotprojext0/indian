f94149dd75671c03219787a4344fbca42fe5f116
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-02-02 15:36:04 +0530

    Update eval.py

--- haruka/modules/eval.py
+++ haruka/modules/eval.py
@@ -148,11 +148,11 @@ def error_callback(bot, update, error):
 __mod_name__ = "Eval Module"
 
 
-EVAL_HANDLER = CommandHandler(('eval', 'py'), eval, filters=CustomFilters.sudo_filter)
+eval_handle = CommandHandler(('eval', 'py'), eval, filters=CustomFilters.sudo_filter)
 exec_handle = CommandHandler(('x', 'ex', 'exe', 'exec', 'py'), execute, filters=CustomFilters.sudo_filter)
 clear_handle = CommandHandler('clearlocals', clear)
 
-dispatcher.add_handler(EVAL_HANDLER)
+dispatcher.add_handler(eval_handle)
 dispatcher.add_handler(exec_handle)
 dispatcher.add_handler(clear_handle)
 #To be uncommented after testing
