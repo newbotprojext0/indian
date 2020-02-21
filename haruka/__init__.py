12a70356046df8a511f23e8af04fcd120e0ea882
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-02-21 15:29:36 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -101,7 +101,7 @@ else:
 
 
 SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.add(254318997)
+SUDO_
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
 
@@ -112,7 +112,7 @@ WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
 
 # Load at end to ensure all prev variables have been set
-from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
+from haruka.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
 
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
