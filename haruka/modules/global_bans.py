135cf1364dcabc3427d012153b3456c629016e6a
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-02-25 00:28:31 +0530

    Update global_bans.py

--- haruka/modules/global_bans.py
+++ haruka/modules/global_bans.py
@@ -7,8 +7,8 @@ from telegram.error import BadRequest, TelegramError
 from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import haruka.modules.sql.global_bans_sql as sql
-from haruka import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, STRICT_GBAN, GBAN_LOGS, WHITELIST_USERS
+import haruka.modules.sql.antispam_sql as sql
+from haruka import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, STRICT_ANTISPAM
 from haruka.modules.helper_funcs.chat_status import user_admin, is_user_admin
 from haruka.modules.helper_funcs.extraction import extract_user, extract_user_and_text
 from haruka.modules.helper_funcs.filters import CustomFilters
