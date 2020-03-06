5b1f80de99f1c760c89be201d9a3d2e40e3ccca1
ThakorRahul <56543994+ThakorRahul@users.noreply.github.com>
2020-03-06 10:20:50 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -135,7 +135,7 @@ def send_start(bot, update):
         pass
 
     #chat = update.effective_chat  # type: Optional[Chat] and unused variable
-    text = "Hey there! My name is Kanna Kobayashi- I'm here to help you manage your groups!\n\
+    text = "Hey *{}! My name is *{}* \nI'm here to help you manage your groups!\n\
 Click Help button to find out more about how to use me to my full potential.\n\n"
 
 
@@ -143,7 +143,7 @@ Click Help button to find out more about how to use me to my full potential.\n\n
     text += "Want to add me to your group? [Click here!](t.me/Kanna_Chan_Bot?startgroup=true)!\n\
     Maintained by [this awesome person](https://t.me/Okay_retard).\n\n"
 
-    keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/LucySupportChat")]]
+    keyboard = [[InlineKeyboardButton(text="➕Add me to a Group ➕", url="http://t.me/SarikaRoBot?startgroup=true")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
     keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
